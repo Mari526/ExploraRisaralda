7d2e005f0163946c6d3863cdc9c4ebe64b5595a9
Mari526 <marianamillan505@gmail.com>
2025-04-24 18:54:03 -0500

    Agrego la estructura y el diseño final de la interfaz de pereira, tres caruseles funcionales

--- municipios/pereira/js/main-pereira.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const carrusel = document.querySelector('#carruselExperiencias');
-    if (carrusel) {
-      // CoreUI automáticamente lo activa si tienes el atributo `data-coreui-ride="carousel"`
-      console.log("Carrusel de experiencias activado");
-    }
-  });
